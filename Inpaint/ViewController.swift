63641eeba4319109de21a35840801917abc48f56
wuximiao <wuximiao@rd.netease.com>
2023-11-30 22:56:04 +0800

    Initial Commit

---
+++ Inpaint/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Inpaint
+//
+//  Created by wudijimao on 2023/11/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

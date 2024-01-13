fb188eee02bd0a09fa31dd856e9861fd7c2bc34c
wudijimao <wudijimao@gmail.com>
2024-01-13 14:20:50 +0800

    add: 增加demo，用来演示protal的用法

--- MagicPhoto/MagicPhotoApp.swift
+++ MagicPhoto/MagicPhotoApp.swift
@@ -6,12 +6,61 @@
 //
 
 import SwiftUI
+import RealityKit
+import RealityKitContent
+
+//@main
+//struct MagicPhotoApp: App {
+//    var body: some Scene {
+//        WindowGroup {
+//            ContentView()
+//        }.windowStyle(.plain)
+//        
+//        WindowGroup(id: "photo") {
+//            MagictPhoto()
+//        }.windowStyle(.volumetric)
+//        
+//    }
+//}
+
+@MainActor
+public func makeWorld() async -> Entity {
+    let world = Entity()
+    world.components[WorldComponent.self] = .init()
+    let child = try! await Entity(named: "Scene", in: realityKitContentBundle)
+    world.addChild(child)
+    
+    return world
+}
+
+public func makePortal(world: Entity) -> Entity {
+    let portal = Entity()
+    
+    portal.components[ModelComponent.self] = .init(mesh: .generatePlane(width: 1,
+                                                                        depth: 1,
+                                                                        cornerRadius: 0.5),
+                                                   materials: [PortalMaterial()])
+    portal.components[PortalComponent.self] = .init(target: world)
+    return portal
+}
 
 @main
 struct MagicPhotoApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }.windowStyle(.plain)
+
+    @State private var immersionStyle: ImmersionStyle = .mixed
+
+    var body: some SwiftUI.Scene {
+        ImmersiveSpace {
+            RealityView { content in
+                let anchor = AnchorEntity(.plane(.vertical, classification: .wall,
+                                                 minimumBounds: [1, 1]))
+                content.add(anchor)
+                let word = await makeWorld()
+                let portal = makePortal(world: word)
+                anchor.addChild(word)
+                anchor.addChild(portal)
+            }
+        }
+        .immersionStyle(selection: $immersionStyle, in: .mixed)
     }
 }
